53744c3d5d6aab1875800ee0d9aaa339be684186
https://github.com/philipsteve-7 <your_email@example.com>
2025-03-11 15:55:30 +0530

    updated all

--- server.js
+++ server.js
@@ -1,63 +1,24 @@
-const express = require('express');
-const cors = require('cors'); // Import CORS
-const mongoose = require('mongoose');
-require('dotenv').config();
-const User = require('./models/User');
-const bcrypt = require('bcryptjs');
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
-const PORT = 5000;
+
 app.use(express.json());
+app.use(cors());
 
-// Enable CORS for the frontend URL
-app.use(cors({
-  origin: 'https://eyrsmain.onrender.com', // The React frontend URL
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
-// Home page API
-app.get('/', (req, res) => {
-  res.send("<h1 align=center>Welcome to the MERN Stack Recipie Platform</h1>");
-});
+mongoose.connect(
+  "mongodb+srv://philipstevechembian:0987654@cluster0.lb8zi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
 
-// Registration page API
-app.post('/register', async (req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, email, password: hashedPassword });
-    await user.save();
-    res.json({ message: "User Registered.." });
-    console.log("User Registration completed...");
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// Login page API
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(400).json({ message: "Invalid Credentials" });
-    }
-    res.json({ message: "Login Successful", username: user.username });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-mongoose.connect(process.env.MONGO_URL).then(
-  () => console.log("DB connected successfully..")
-).catch(
-  (err) => console.log(err)
-);
-
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("Server is running on port :" + PORT);
-});
+app.listen(3001, () => console.log("Server started"));
