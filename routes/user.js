6ec139aee3f013f99e51ecb0179c7986df1796f8
https://github.com/philipsteve-7 <your_email@example.com>
2025-03-12 00:50:10 +0530

    all updates1

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs';
+
 
 const router = express.Router();
 import { UserModel } from "../models/Users.js";
